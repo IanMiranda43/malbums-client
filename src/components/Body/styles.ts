4f5f1123e9755debed91fd76629514883fd5d357
Ian Miranda <miranda.dev43@gmail.com>
2021-10-06 20:02:25 -0300

    refactor: improve responsivity

--- src/components/Body/styles.ts
+++ src/components/Body/styles.ts
@@ -2,8 +2,11 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
-  margin: auto 0;
   padding: 20px;
+
+  @media (max-width: 550px) {
+    margin: auto 0;
+  }
 
   @media (min-width: 551px) {
     padding: 40px;
